e554ddbc9cf5a4e7ada98c5f6048bc361ace6a4c
Mohamed Salem <mohamedsalemcs@gmail.com>
2021-08-21 19:38:42 +0200

    styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,28 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { Language } from 'src/app/modules/core/enums/language';
-
+import { Language } from "src/app/modules/core/enums/language";
 
 export const environment = {
   production: false,
 
-  dabernyServerUrl: 'https://localhost:44358/',
-  apiUrl: 'https://localhost:44358/api',
-  // dabernyServerUrl: 'http://192.168.1.5:2021/',
-  // apiUrl: 'http://192.168.1.5:2021/api',
-  langKey: 'lang',
-  tokenKey: 'token',
-  loginProviderKey: 'loginProvider',
+  dabernyServerUrl: "https://localhost:44358/",
+  apiUrl: "https://localhost:44358/api",
+
+  // dabernyServerUrl: "http://52.15.72.219:5050/",
+  // apiUrl: "http://52.15.72.219:5050/api",
+
+  // dabernyServerUrl: "http://192.168.1.5:2021/",
+  // apiUrl: "http://192.168.1.5:2021/api",
+
+  langKey: "lang",
+  tokenKey: "token",
+  loginProviderKey: "loginProvider",
   defaultLanguage: Language.Arabic,
-  maxImageSize: 1000 * 1000 * 5
+  maxImageSize: 1000 * 1000 * 5,
 };
 export const loading = {
-  showLoading: false
+  showLoading: false,
 };
 
 /*
